b68b60fb3638417599cf8ac0c0ae83457b665c35
Paul Marchand <paulmarchand321@gmail.com>
2021-04-24 17:32:55 -0400

    updated contact page responsiveness

--- realest/page-contact.php
+++ realest/page-contact.php
@@ -8,17 +8,16 @@ get_header();
 
 			<div id="contact_section_1">
 				<div id="contact_sub_1">
-
 				</div>
 				<div id="contact_sub_2">
 					<h1>linkup we cool<br>talk to us</h1>
 					<p>For any information, collaboration, coverage, partnership please contact us
 					by email, via instagram or directly through this contact form.</p>
 					<div id="newsletter">
-						<input type="text" id="user-name" name="name" placeholder="NAME">
-						<input type="text" id="user-email" name="email" placeholder="EMAIL">
+						<input type="text" id="user-name-contact" name="name" placeholder="NAME">
+						<input type="text" id="user-email-contact" name="email" placeholder="EMAIL">
 					</div>
-					<a class="promo-cta"  href="#">Submit</a>
+					<a class="contact-cta"  href="#">Submit</a>
 				</div>
 				</div>
 			</div>
